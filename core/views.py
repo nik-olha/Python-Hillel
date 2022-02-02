2c1779faf16193f7ff78ce2dc216e47cd6f8032e
niolha <olhanikulina2@gmail.com>
2022-02-03 00:09:31 +0100

    setup templates

--- core/views.py
+++ core/views.py
@@ -1,1 +1,13 @@
-# from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
+from django.views.generic import ListView
+
+# def posts(request):
+#     posts = Post.objects.all()
+#     results = ", ".join([post.title for post in posts])
+#     return HttpResponse(results)
+
+
+class PostView(ListView):
+    template_name = 'core/posts.html'
+    queryset = Post.objects.all()
