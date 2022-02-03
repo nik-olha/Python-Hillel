e94147da845972c74cc78d528c6be770e8de9dd8
niolha <olhanikulina2@gmail.com>
2022-02-03 18:10:44 +0100

    add Post-Details Page

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import PostView
+from .views import PostView, PostDetailView
 
 
 urlpatterns = [
-    path("posts/", PostView.as_view())
+    path("posts/", PostView.as_view()),
+    path("posts/<int:pk>/", PostDetailView.as_view())
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,8 @@
 # from django.http import HttpResponse
-from .models import Post
-from django.views.generic import ListView
+from .models import Post, Like
+from django.db.models import query
+from django.views.generic import TemplateView
+from django.views.generic import ListView, DetailView
 
 # def posts(request):
 #     posts = Post.objects.all()
@@ -11,3 +13,32 @@ from django.views.generic import ListView
 class PostView(ListView):
     template_name = 'core/posts.html'
     queryset = Post.objects.all()
+    # context_object_name = 'posts'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        posts = Post.objects.all()
+        results = [
+            (
+                p, 
+                p.like_set.filter(status=True).count(),
+                p.like_set.filter(status=False).count()
+            ) 
+                for p in posts
+        ]
+        context['results'] = results
+        return context
+    
+class PostDetailView(DetailView):
+    queryset = Post.objects.all()
+    template_name = 'core/post.html'
+
+
+class PostDeleteView():
+    pass
+
+class PostUpdateView():
+    pass
+
+class PostCreateView():
+    pass
