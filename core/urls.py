eb46d5471f5753f1782d7ff34e43058b3d228109
niolha <olhanikulina2@gmail.com>
2022-02-03 22:34:08 +0100

    try to add PostDeleteView

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import PostView, PostDetailView
+from .views import PostView, PostDetailView, PostDeleteView
 
 
 urlpatterns = [
     path("posts/", PostView.as_view()),
-    path("posts/<int:pk>/", PostDetailView.as_view(), name='post_detail')
+    path("posts/<int:pk>/", PostDetailView.as_view(), name='post_detail'),
+    path('posts/<int:pk>/delete/', PostDeleteView.as_view())
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,6 @@
 from .models import Post, Like
 from django.db.models import query
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, DeleteView
 
 
 class PostView(ListView):
@@ -12,15 +12,16 @@ class PostView(ListView):
         posts = Post.objects.all()
         results = [
             (
-                p, 
+                p,
                 p.like_set.filter(status=True).count(),
                 p.like_set.filter(status=False).count()
-            ) 
-                for p in posts
+            )
+            for p in posts
         ]
         context['results'] = results
         return context
-    
+
+
 class PostDetailView(DetailView):
     queryset = Post.objects.all()
     template_name = 'core/post.html'
@@ -29,17 +30,20 @@ class PostDetailView(DetailView):
         context = super().get_context_data(**kwargs)
         posts = Post.objects.all()
         post = self.object
-        
+
         context['likes'] = post.like_set.filter(status=True).count()
         context['dislikes'] = post.like_set.filter(status=False).count()
         return context
-    
 
-class PostDeleteView():
-    pass
+
+class PostDeleteView(DeleteView):
+    model = Post
+    success_url = "/"
+
 
 class PostUpdateView():
     pass
+
 
 class PostCreateView():
     pass
