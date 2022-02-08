d851b0674834bd19109987f8a3bfb550b061a3dc
niolha <olhanikulina2@gmail.com>
2022-02-08 23:15:57 +0100

    add styles to new html-pages

--- core/forms.py
+++ core/forms.py
@@ -4,13 +4,14 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ["title", "content"]
 
-    def save(self, commit = True): 
-        post = super().save(commit = False)
+    def save(self, commit=True):
+        post = super().save(commit=False)
         # setattr(post, 'user', User.objects.first())
         post.user = User.objects.first()
         if commit:
